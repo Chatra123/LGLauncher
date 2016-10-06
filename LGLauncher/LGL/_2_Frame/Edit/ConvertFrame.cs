c3ed261b2640f8c9d869c234dcc97384feb3a069
c <>
2016-10-06 20:31:14 +0900

    ConvertFrame整理

--- LGLauncher/LGL/_2_Frame/Edit/ConvertFrame.cs
+++ LGLauncher/LGL/_2_Frame/Edit/ConvertFrame.cs
@@ -15,7 +15,7 @@ namespace LGLauncher.Frame
   /// <summary>
   /// フレームリストから各形式への変換
   /// </summary>
-  static class ConvertFrame
+  public static class ConvertFrame
   {
     /// <summary>
     /// read *.frame.txt  -->  List<int>
@@ -26,7 +26,6 @@ namespace LGLauncher.Frame
     /// </returns>
     public static List<int> Read_FrameFile(string framePath)
     {
-      //check
       if (File.Exists(framePath) == false) return null;
 
       //読
@@ -69,24 +68,23 @@ namespace LGLauncher.Frame
     /// 短いＭａｉｎをつぶす
     /// </summary>
     /// <param name="frameList">元になるフレームリスト</param>
-    /// <param name="ths_Mainsec">指定秒数以下のＭａｉｎをつぶす</param>
+    /// <param name="mini_main_sec">指定秒数以下のＭａｉｎをつぶす</param>
     /// <returns>
     ///   成功  -->  List<int>
     ///   失敗  -->  null
     /// </returns>
-    public static List<int> FlatOut_Main(List<int> frameList, double ths_Mainsec)
+    public static List<int> FlatOut_Main(List<int> frameList, double mini_main_sec)
     {
-      //エラーチェック
       if (frameList == null) return null;
       if (frameList.Count % 2 == 1) return null;
       if (frameList.Count <= 2) return frameList;
 
       var newList = new List<int>();
+      int mini_main = (int)(mini_main_sec * 29.970);// sec --> frame
       for (int i = 0; i < frameList.Count; i += 2)
       {
-        double mainLength = 1.0 * (frameList[i + 1] - frameList[i]) / 29.970;
-
-        if (ths_Mainsec < mainLength)
+        double mainLen = frameList[i + 1] - frameList[i];
+        if (mini_main < mainLen)
         {
           newList.Add(frameList[i]);
           newList.Add(frameList[i + 1]);
@@ -101,48 +99,45 @@ namespace LGLauncher.Frame
     /// 短いＣＭをつぶす
     /// </summary>
     /// <param name="frameList">元になるフレームリスト</param>
-    /// <param name="ths_CMsec">指定秒数以下のＣＭをつぶす</param>
+    /// <param name="mini_cm_sec">指定秒数以下のＣＭをつぶす</param>
     /// <returns>
     ///   成功  -->  List<int>
     ///   失敗  -->  null
     /// </returns>
     /// <remarks>開始直後のＣＭはつぶさない。</remarks>
-    public static List<int> FlatOut_CM__(List<int> frameList, double ths_CMsec)
+    public static List<int> FlatOut_CM__(List<int> frameList, double mini_cm_sec)
     {
       if (frameList == null) return null;
       if (frameList.Count % 2 == 1) return null;
       if (frameList.Count <= 2) return frameList;
 
+      //「frameList[i]」と「newListの末尾」の差が CM Length
       //
-      //「frameList[i]のフレーム数」と「newListの末尾のフレーム数」の差がcmLength
-      //
-      //cmLengthが短ければ newList[last]を次の本編終端 frameList[i+1]に置き換える。
-      //　　　　　長ければ newListに本編 frameList[i] to frameList[i+1]を加える。
-      //
-      //ただし、開始直後のＣＭは無視する。
+      // CM Lengthが長ければ newListに本編 frameList[i], frameList[i+1]を加える。
+      //　　　　　　短ければ newList[last]を次の本編終端 frameList[i + 1]に置き換え短いＣＭをつぶす。
+      //ただし、開始直後のＣＭはそのまま、
       //開始直後に短いＣＭがあっても本編には加えない。
-      //
       var newList = new List<int>();
+      int mini_cm = (int)(mini_cm_sec * 29.970);// sec --> frame
 
-      //最初のmain
+      //1st main
       newList.Add(frameList[0]);
       newList.Add(frameList[1]);
-
       for (int i = 2; i < frameList.Count; i += 2)
       {
-        double cmLength = 1.0 * (frameList[i] - newList.Last()) / 29.970;
-        if (cmLength < ths_CMsec)
-        {
-          //短
-          //ＣＭを無視し本編内とする。次の本編終端に置き換える
-          newList[newList.Count - 1] = frameList[i + 1];
-        }
-        else
+        int cmLen = frameList[i] - newList.Last();
+        if (mini_cm < cmLen)
         {
           //長
           //ＣＭを採用し通常の　本編始端＆終端　を加える
           newList.Add(frameList[i]);
           newList.Add(frameList[i + 1]);
+        }
+        else
+        {
+          //短
+          //ＣＭを無視し本編内とする。次の本編終端に置き換え短いＣＭをつぶす。
+          newList[newList.Count - 1] = frameList[i + 1];
         }
       }
 
