94d8649b256cc1f4e7bd54ff3d2b7bcd72978bce
c <>
2016-10-08 22:42:48 +0900

    tweakベースファイルも再出力するように変更

--- TweakFrame/Program.cs
+++ TweakFrame/Program.cs
@@ -36,22 +36,22 @@ namespace TweakFrame
       //tweak
       const double Regard_MainAsCm__Side = 20.0;
       const double Regard_CmAsMain__Side = 14.0;
-      const double Regard_MainAsCm__Center = 90.0;
-      const double Regard_CmAsMain__Center = 55.0;
+      const double Regard_MainAsCm__Center = 60.0;
+      const double Regard_CmAsMain__Center = 40.0;
 
-      double total = frame.Last() + 29.970 * 60;
+      double total = frame.Last() + 29.970 * 60;//推定
       double first_10min = 30 * 60 * 10, Last_10min = total - 30 * 60 * 10;//in frames
 
       var tweak = frame.ToList();//copy
       for (int i = 0; i < 2; i++)
       {
-        tweak = Tweaker.FlatOut_Main(tweak, Regard_MainAsCm__Side, 0, first_10min);
-        tweak = Tweaker.FlatOut_Main(tweak, Regard_MainAsCm__Center, first_10min, Last_10min);
-        tweak = Tweaker.FlatOut_Main(tweak, Regard_MainAsCm__Side, Last_10min, total);
-
         tweak = Tweaker.FlatOut_CM__(tweak, Regard_CmAsMain__Side, 0, first_10min);
         tweak = Tweaker.FlatOut_CM__(tweak, Regard_CmAsMain__Center, first_10min, Last_10min);
         tweak = Tweaker.FlatOut_CM__(tweak, Regard_CmAsMain__Side, Last_10min, total);
+
+        tweak = Tweaker.FlatOut_Main(tweak, Regard_MainAsCm__Side, 0, first_10min);
+        tweak = Tweaker.FlatOut_Main(tweak, Regard_MainAsCm__Center, first_10min, Last_10min);
+        tweak = Tweaker.FlatOut_Main(tweak, Regard_MainAsCm__Side, Last_10min, total);
       }
       SideTool.ShowFrame(frame, "file");
       SideTool.ShowFrame(tweak, "tweak");
@@ -64,21 +64,26 @@ namespace TweakFrame
       bool isDiff = 3.0 * 29.970 < diff;    // Nsec以上の変化がある
       if (isDiff)
       {
-        string[] frameText = tweak.Select(f => f.ToString()).ToArray();
-        string tvtpText = ConvertFrame.To_TvtpChap(tweak);
         string dir = Path.GetDirectoryName(path);
         string name = Path.GetFileName(path);                                  //sample.frame.txt
         string basename = name.Substring(0, name.Length - ".frame.txt".Length);//sample
         //frame
         {
-          string new_path = Path.Combine(dir, "tweakF_" + name);
-          File.WriteAllLines(new_path, frameText);
+          string[] org_frameText = frame.Select(f => f.ToString()).ToArray();
+          string[] twk_frameText = tweak.Select(f => f.ToString()).ToArray();
+          string org_path = Path.Combine(dir, "Origin_" + basename + ".frame.txt");
+          string twk_path = Path.Combine(dir, "Tweak__" + basename + ".frame.txt");
+          File.WriteAllLines(org_path, org_frameText);
+          File.WriteAllLines(twk_path, twk_frameText);
         }
         //tvtp
         {
-          string chap_name = basename + ".chapter";
-          string new_path = Path.Combine(dir, "tweakF_" + chap_name);
-          File.WriteAllLines(new_path, frameText);
+          string org_tvtpText = ConvertFrame.To_TvtpChap(frame);
+          string twk_tvtpText = ConvertFrame.To_TvtpChap(tweak);
+          string org_path = Path.Combine(dir, "Origin_" + basename + ".chapter");
+          string twk_path = Path.Combine(dir, "Tweak__" + basename + ".chapter");
+          File.WriteAllText(org_path, org_tvtpText);
+          File.WriteAllText(twk_path, twk_tvtpText);
         }
       }
     }
